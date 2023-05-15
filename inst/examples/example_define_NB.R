9e8632df5164f6d2866207eb721b277bc43fc1ef
rjaffe123 <rachael.jaffe@mail.utoronto.ca>
2023-05-14 20:07:39 -0400

    updated description

--- inst/examples/example_define_NB.R
+++ inst/examples/example_define_NB.R
@@ -1,10 +1,9 @@
 data(data_example, package = "personlevelEE")
 
-cost_object <- define_cost(data_example$cost,
-                           data_example$id, data_example$tx)
 
-effect_object <- define_effect(data_example$effect,
-                               data_example$id, data_example$tx)
+cost_object <- define_cost(data_example, cost = cost, id = id, tx = tx)
+
+effect_object <- define_effect(data_example, effect = effect, id = id, tx = tx)
 
 nb_value <- define_NB(lambdas = c(100, 350, 1000, 10000),
                       cost = cost_object, effect = effect_object)
